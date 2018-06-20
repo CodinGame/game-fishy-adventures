04781fc2d48471b5d74dbb56b02835e58383f53f
Thomas <t.foucault2@gmail.com>
2018-06-20 09:19:48 +0200

    Cleanup unused multi setup

--- src/main/java/com/codingame/game/Referee.java
+++ src/main/java/com/codingame/game/Referee.java
@@ -1,13 +1,11 @@
 package com.codingame.game;
 import com.codingame.gameengine.core.AbstractReferee;
-import com.codingame.gameengine.core.MultiplayerGameManager;
+import com.codingame.gameengine.core.SoloGameManager;
 import com.codingame.gameengine.module.entities.GraphicEntityModule;
 import com.google.inject.Inject;
 
 public class Referee extends AbstractReferee {
-    // Uncomment the line below and comment the line under it to create a Solo Game
-    // @Inject private SoloGameManager<Player> gameManager;
-    @Inject private MultiplayerGameManager<Player> gameManager;
+    @Inject private SoloGameManager<Player> gameManager;
     @Inject private GraphicEntityModule graphicEntityModule;
 
     @Override
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,27 +1,14 @@
-import com.codingame.gameengine.runner.MultiplayerGameRunner;
+import com.codingame.gameengine.runner.SoloGameRunner;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Uncomment this section and comment the other one to create a Solo Game
-        /* Solo Game */
-        // SoloGameRunner gameRunner = new SoloGameRunner();
+        SoloGameRunner gameRunner = new SoloGameRunner();
 
         // Sets the player
-        // gameRunner.setAgent(Player1.class);
+        gameRunner.setAgent(Player1.class);
 
         // Sets a test case
-        // gameRunner.setTestCase("test1.json");
-
-        /* Multiplayer Game */
-        MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
-
-        // Adds as many player as you need to test your game
-        gameRunner.addAgent(Player1.class);
-        gameRunner.addAgent(Player2.class);
-
-        // Another way to add a player
-        // gameRunner.addAgent("python3 /home/user/player.py");
+        gameRunner.setTestCase("test1.json");
 
         gameRunner.start();
     }
