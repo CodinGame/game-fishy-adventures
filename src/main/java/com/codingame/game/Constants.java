0999f8d21783834dec0cc9e5edc7b7f683183928
Thomas <t.foucault2@gmail.com>
2018-06-22 10:38:42 +0200

    Use World constants

--- src/main/java/com/codingame/game/Constants.java
+++ src/main/java/com/codingame/game/Constants.java
@@ -3,9 +3,11 @@ package com.codingame.game;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.codingame.gameengine.module.entities.World;
+
 public class Constants {
-    public static final int VIEWER_WIDTH = 1920;
-    public static final int VIEWER_HEIGHT = 1020;
+    public static final int VIEWER_WIDTH = World.DEFAULT_WIDTH;
+    public static final int VIEWER_HEIGHT = World.DEFAULT_HEIGHT;
     
     public static final int CELL_SIZE = 128;
     public static final int CELL_OFFSET = CELL_SIZE / 2;
